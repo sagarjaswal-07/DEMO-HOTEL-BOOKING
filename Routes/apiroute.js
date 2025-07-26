f15aa0a8373b50898ae70e80d11834dfe21d53b5
SagarJaswal <dheeraj07.in@gmail.com>
2025-07-26 16:54:43 +0530

    updated route file

--- Routes/apiroute.js
+++ Routes/apiroute.js
@@ -5,17 +5,17 @@ const roomController = require('../Server/Rooms/roomController');
 const managerController = require('../Server/Manager/managerController');
 
 //queries
-router.post('/query/add',queryController.add)
-router.post('/query/getall',queryController.getall)
-router.post('/query/getsingle',queryController.getsingle)
+router.get('/query/add',queryController.add)
+router.get('/query/getall',queryController.getall)
+router.get('/query/getsingle',queryController.getsingle)
 //type
-router.post('/type/add',roomTypeController.add)
-router.post('/type/getall',roomTypeController.getall)
+router.get('/type/add',roomTypeController.add)
+router.get('/type/getall',roomTypeController.getall)
 //room
-router.post('/room/add',roomController.add)
-router.post('/room/getall',roomController.getall)
-router.post('/room/getsingle',roomController.getsingle)
+router.get('/room/add',roomController.add)
+router.get('/room/getall',roomController.getall)
+router.get('/room/getsingle',roomController.getsingle)
 //manager
-router.post('/manager/register',managerController.register)
+router.get('/manager/register',managerController.register)
 
 module.exports = router ;
--- index.js
+++ index.js
@@ -9,6 +9,11 @@ app.use(express.json({limit:'50mb'}))
 const routes = require('./Routes/apiroute')
 app.use('/api',routes)
 
+app.get('/',(req,res)=>{
+  res.send('Backend is Running')
+  
+})
+
 app.listen(port,()=>{
   console.log('My App is Running on Port No.'+' '+port);
 })
