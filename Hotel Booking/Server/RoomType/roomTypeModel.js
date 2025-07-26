68b665f793d887d721783b8fb607b4b98640811c
SagarJaswal <dheeraj07.in@gmail.com>
2025-07-26 16:14:44 +0530

    Delete Useless Booking directory

--- Hotel Booking/Server/RoomType/roomTypeModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const roomTypeSchema = new mongoose.Schema({
-  roomTypeName : {type:String,default:null},
-  description : {type:String,default:null},
-  status : {type:String,default:'Active'},
-  createdAt : {type:Date,default:Date.now()},
-})
-
-module.exports = new mongoose.model('roomTypes',roomTypeSchema);
