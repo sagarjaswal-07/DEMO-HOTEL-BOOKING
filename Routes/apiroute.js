0756c67f2f85ad6985e6cd512c82198d778775ac
SagarJaswal <dheeraj07.in@gmail.com>
2025-07-26 16:57:52 +0530

    Updated Route Directory for Render

--- Routes/apiroute.js
+++ Routes/apiroute.js
@@ -5,17 +5,20 @@ const roomController = require('../Server/Rooms/roomController');
 const managerController = require('../Server/Manager/managerController');
 
 //queries
-router.get('/query/add',queryController.add)
+router.post('/query/add',queryController.add)
+router.post('/query/getall',queryController.getall)
 router.get('/query/getall',queryController.getall)
-router.get('/query/getsingle',queryController.getsingle)
+router.post('/query/getsingle',queryController.getsingle)
 //type
-router.get('/type/add',roomTypeController.add)
+router.post('/type/add',roomTypeController.add)
+router.post('/type/getall',roomTypeController.getall)
 router.get('/type/getall',roomTypeController.getall)
 //room
-router.get('/room/add',roomController.add)
+router.post('/room/add',roomController.add)
+router.post('/room/getall',roomController.getall)
 router.get('/room/getall',roomController.getall)
-router.get('/room/getsingle',roomController.getsingle)
+router.post('/room/getsingle',roomController.getsingle)
 //manager
-router.get('/manager/register',managerController.register)
+router.post('/manager/register',managerController.register)
 
 module.exports = router ;
